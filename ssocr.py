180774ca97549cdd2d9ad11a21ef82f1a0e9681c
Just_A_Lonely_Porter <76800139+LonelyPorter@users.noreply.github.com>
2022-03-12 15:34:55 -0500

    Added multithreading and fixed Bug
    Fixed bug that process not responding(halts) when capture(screenshot) fails.

--- ssocr.py
+++ ssocr.py
@@ -4,61 +4,81 @@ from PIL import ImageGrab
 from time import sleep
 from ctypes import windll
 from easyocr import Reader
+from queue import Queue
+from threading import Thread
+import psutil as ps
 
-def ocr_capture():
+def checkProcessRunning(proc_name:str):
+    for proc in ps.process_iter():
+        if proc_name.lower() in proc.name().lower():
+            return True
+    return False
+
+def ocr_capture(q:Queue):
     reader = Reader(['ja'], gpu=True)
-    text = reader.readtext("paste.png", paragraph=True)
+    while True:
+        try:
+            text = reader.readtext("paste.png", paragraph=True)
+            text = " ".join((t[1] for t in text))
+            q.put(text)
+            # sleep because the png not get deleted fast enough and will put bunch of text into the queue
+            sleep(5)
+        except:
+            sleep(2)
 
-    return " ".join((t[1] for t in text))
 
 layout = [  
     [sg.Image(key="-IMAGE-")],
-    [[sg.Text(size=(40, 1), key="-TOUT-")], [sg.Button('Copy')]],
+    [[sg.Text(size=(40, 5), key="-TOUT-")], [sg.Button('Copy')]],
     [sg.Button('Capture'), sg.Button('Quit')] 
 ]
 
 margins=(200, 50)
 
-# Create the Window
-window = sg.Window('Text Capture', layout, margins=margins)
-text = ""
+if __name__ == '__main__':
+    # Create the Window
+    window = sg.Window('Text Capture', layout, margins=margins)
+    text = ""
+    q = Queue()
+    t1 = Thread(target=ocr_capture, args=(q,))
+    t1.setDaemon(True)
+    t1.start()
 
-while True:
-    event, values = window.read()
-     # if user closes window or clicks cancel
-    if event == sg.WIN_CLOSED or event == 'Quit':
-        break
-    elif event == 'Capture':
-        # clear clipboard
-        if windll.user32.OpenClipboard(None):
-            windll.user32.EmptyClipboard()
-            windll.user32.CloseClipboard()
+    while True:
+        event, values = window.read()
+        # if user closes window or clicks cancel
+        if event == sg.WIN_CLOSED or event == 'Quit':
+            break
+        elif event == 'Capture':
+            # clear clipboard
+            if windll.user32.OpenClipboard(None):
+                windll.user32.EmptyClipboard()
+                windll.user32.CloseClipboard()
 
-        img = None
-        if os.system('explorer ms-screenclip:'):
-            while not img:
-                try:
-                    img = ImageGrab.grabclipboard()
-                except OSError:
-                    sleep(1)
+            img = None
+            if os.system('explorer ms-screenclip:'):
+                while checkProcessRunning("ScreenClippingHost.exe"):
+                    pass
+                
+                img = ImageGrab.grabclipboard()
+                if img:
+                    # save the image to be processed
+                    img.save('paste.png', 'PNG')
 
-            # save the image to be processed
-            img.save('paste.png', 'PNG')
+                    # update the image on the window
+                    text = q.get()
+                    window["-TOUT-"].update(text)
+                    window["-IMAGE-"].update(filename='paste.png')
 
-            # update the image on the window
-            text = ocr_capture()
-            window["-TOUT-"].update(text)
-            window["-IMAGE-"].update(filename='paste.png')
+            else:
+                print("Fail to open snipping tool")
 
-        else:
-            print("Fail to open snipping tool")
+        elif event == 'Copy':
+            print(text)
+            sg.clipboard_set(text)
+            continue
 
-    elif event == 'Copy':
-        print(text)
-        sg.clipboard_set(text)
+        if os.path.exists("paste.png"):
+            os.remove("paste.png")
 
-
-    if os.path.exists("paste.png"):
-        os.remove("paste.png")
-
-window.close()
+    window.close()
